b10e728efcbabfa1801c7fd92f23aa292368be75
thaihn <thaihn@ivstech.co.kr>
2025-04-22 14:27:13 +0700

    update code KK

--- models/face_registered.py
+++ models/face_registered.py
@@ -6,7 +6,7 @@ class Face(Base):
     __tablename__ = 'face_registered'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100), nullable=True)  # Thêm cột name với độ dài 100 ký tự
+    userName = Column(String(100), nullable=True)  # Thêm cột name với độ dài 100 ký tự
     direction = Column(String(50), nullable=True)
     faceInfoId = Column(Integer, ForeignKey('face_information.faceInfoId'), nullable=True)
     embedding = Column(LargeBinary(2048), nullable=True)  # Specified size of 2048 bytes
